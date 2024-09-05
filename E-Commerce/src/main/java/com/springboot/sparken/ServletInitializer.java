61e6347ec9b9b378e66aece01ce6d43454d0d27f
Sabihajamadar <sabihajamadar5@gmail.com>
2024-09-05 17:27:22 +0530

    test commit

---
+++ E-Commerce/src/main/java/com/springboot/sparken/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.springboot.sparken;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ECommerceApplication.class);
+	}
+
+}
